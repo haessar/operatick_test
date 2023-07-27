ecb7e177b46100e51a7335ed2be53f4a3a5d6ac6
Will Haese-Hill <wahhill@gmail.com>
2023-07-27 20:39:56 +0100

    specify Opera model for OperaFilter

--- filters.py
+++ filters.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 import django_filters
+
+from .models import Opera
 from melodramatick.work.filters import WorkFilter
 
 
@@ -7,4 +9,5 @@ class OperaFilter(WorkFilter):
     language = django_filters.ChoiceFilter(choices=settings.LANGUAGE_CHOICES)
 
     class Meta(WorkFilter.Meta):
+        model = Opera
         fields = ['composer', 'composer_group', 'language', 'top_list', 'duration_range', 'genre']
